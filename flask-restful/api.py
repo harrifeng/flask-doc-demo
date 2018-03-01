617af2875ee0bc7b4ec7feaeb0fd0caeee39548b
Haoran Feng <harrifeng@gmail.com>
2018-03-01 16:03:55 +0800

    ok by now

--- flask-restful/api.py
+++ flask-restful/api.py
@@ -1,13 +1,28 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask.ext.mysql import MySQL
 
+mysql = MySQL()
 app = Flask(__name__)
+app.config.from_pyfile('database.cfg')
+mysql.init_app(app)
 api = Api(app)
 
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        connection = mysql.connect()
+        cursor = connection.cursor()
+        cursor.execute('''SELECT * FROM layer''')
+        ret = cursor.fetchall()
+        layers = []
+        for entry in ret:
+            record = {
+                'id': entry[0],
+                'name': entry[1],
+            }
+            layers.append(record)
+        return layers
 
 
 api.add_resource(HelloWorld, '/')
