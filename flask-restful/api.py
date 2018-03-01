a5bb9bd50016685bbd256ecb9a428a1329407edf
Haoran Feng <harrifeng@gmail.com>
2018-03-01 20:07:09 +0800

    now ok

--- flask-restful/api.py
+++ flask-restful/api.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from peewee import MySQLDatabase, Model, CharField
+from peewee import *
 
 app = Flask(__name__)
 app.config.from_pyfile('database.cfg')
-database = MySQLDatabase('another', user='root', password='rootpass',
+database = MySQLDatabase('abtest', user='root', password='rootpass',
                          host='127.0.0.1', port=3306)
 
 
@@ -15,8 +15,13 @@ class BaseModel(Model):
 
 class Layer(BaseModel):
     class Meta:
-        table_name = 'user'
+        table_name = 'layer'
     name = CharField()
+    status = SmallIntegerField()
+    product_id = IntegerField()
+    params = TextField()
+    create_time = DateTimeField()
+    update_time = DateTimeField()
 
     def foo(self):
         return (Layer.select())
@@ -30,7 +35,17 @@ class HelloWorld(Resource):
         ret = Layer.select()
         layers = []
         for entry in ret:
-            layers.append({entry.id: entry.name})
+            layers.append(
+                {
+                    "id": entry.id,
+                    "name": entry.name,
+                    "status": entry.status,
+                    "product_id": entry.product_id,
+                    "params": entry.params,
+                    # "create_time": entry.create_time,
+                    # "update_time": entry.update_time,
+                }
+            )
         return layers
 
 
