67be928de661138f31a4fae76d5e9dcffc8b55fa
Haoran Feng <harrifeng@gmail.com>
2018-03-01 17:45:47 +0800

    ok version

--- flask-restful/api.py
+++ flask-restful/api.py
@@ -1,27 +1,45 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask.ext.mysql import MySQL
+from peewee import *
 
-mysql = MySQL()
+# mysql = MySQL()
 app = Flask(__name__)
 app.config.from_pyfile('database.cfg')
-mysql.init_app(app)
+# mysql.init_app(app)
+database = MySQLDatabase('another', user='root', password='rootpass',
+                         host='127.0.0.1', port=3306)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Layer(BaseModel):
+    class Meta:
+        table_name = 'user'
+    name = CharField()
+
+    def foo(self):
+        return (Layer.select())
+
+
 api = Api(app)
 
 
 class HelloWorld(Resource):
     def get(self):
-        connection = mysql.connect()
-        cursor = connection.cursor()
-        cursor.execute('''SELECT id, name FROM layer''')
-        ret = cursor.fetchall()
+        # connection = mysql.connect()
+        # cursor = connection.cursor()
+        # cursor.execute('''SELECT id, name FROM layer''')
+        # ret = cursor.fetchall()
+        # ret = Layer.get(Layer.id == 1).foo()
+        ret = Layer.select()
         layers = []
         for entry in ret:
-            record = {
-                'id': entry[0],
-                'name': entry[1],
-            }
-            layers.append(record)
+            print(dir(entry))
+            layers.append({entry.id: entry.name})
         return layers
 
 
